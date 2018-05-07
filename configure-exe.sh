be0276b3e552cc10d69f86d54179c514363be28e
panpan <panpan@zed.com>
2018-05-07 10:25:00 +0800

    分别增加编译可执行ffmpeg、ffplay，和增加静动态库的shell脚本

---
+++ configure-exe.sh
@@ -0,0 +1,3 @@
+./configure --enable-libx264  --enable-gpl --enable-shared --prefix=/home/panpan/player/newest_ffmpeg/ffmpeg/test/
+
+
---
+++ configure-share-stalic.sh
@@ -0,0 +1,3 @@
+./configure --enable-libx264 --enable-libxvid --disable-yasm --disable-asm --enable-static --enable-shared --enable-gpl --enable-version3 --enable-nonfree --enable-ffplay --prefix=/home/panpan/player/newest_ffmpeg/ffmpeg/test/
+
+
